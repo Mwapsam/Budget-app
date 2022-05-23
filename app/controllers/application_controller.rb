10852a945c0687f108e7b21b255fee56035cd0a5
Samuel Mwape <43408618+Mwapsam@users.noreply.github.com>
2022-05-23 11:09:04 +0200

    Aded devise config to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+    protect_from_forgery prepend: true
+    before_action :authenticate_user!
+
+    before_action :update_allowed_parameters, if: :devise_controller?
+
+    protected
+
+    def update_allowed_parameters
+        devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :posts_counter, :email, :password) }
+        devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :email, :password, :current_password) }
+    end
 end
