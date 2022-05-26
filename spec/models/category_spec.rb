533a9a0d6a208749942aba7fe37862713f749459
Samuel Mwape <43408618+Mwapsam@users.noreply.github.com>
2022-05-26 18:05:30 +0200

    Category validation - Correct attributes

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -7,7 +7,9 @@ RSpec.describe Category, type: :model do
     end
     
     describe "Category validations" do
-        
+        it "is valid with valid attributes" do
+            expect(@category).to be_valid
+        end
         
     end
 end
