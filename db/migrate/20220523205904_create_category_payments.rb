dca00cf192be620a387259a0ebaa471bbee31204
Samuel Mwape <43408618+Mwapsam@users.noreply.github.com>
2022-05-26 13:53:40 +0200

    Create category payment

---
+++ db/migrate/20220523205904_create_category_payments.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+class CreateCategoryPayments < ActiveRecord::Migration[7.0]
+  def change
+    create_table :category_payments do |t|
+      t.references :category, null: false, foreign_key: true
+      t.references :payment, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
