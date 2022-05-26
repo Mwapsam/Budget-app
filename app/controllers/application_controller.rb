56137facca2fa5438f104f4db79625d4e41f6092
Samuel Mwape <43408618+Mwapsam@users.noreply.github.com>
2022-05-26 10:12:32 +0200

    Configure application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,15 @@
+# frozen_string_literal: true
+
 class ApplicationController < ActionController::Base
-    protect_from_forgery prepend: true
-    before_action :authenticate_user!
+  protect_from_forgery prepend: true
+  before_action :authenticate_user!
 
-    before_action :update_allowed_parameters, if: :devise_controller?
+  before_action :update_allowed_parameters, if: :devise_controller?
 
-    protected
+  protected
 
-    def update_allowed_parameters
-        devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :posts_counter, :email, :password) }
-        devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :email, :password, :current_password) }
-    end
+  def update_allowed_parameters
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :posts_counter, :email, :password) }
+    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :email, :password, :current_password) }
+  end
 end
