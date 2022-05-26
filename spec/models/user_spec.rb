a1654f9a37069313c14816a43a2e331ec3427549
Samuel Mwape <43408618+Mwapsam@users.noreply.github.com>
2022-05-26 17:48:01 +0200

    Validates if user has password

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,14 +9,20 @@ RSpec.describe User, :type => :model do
     it "is valid with valid attributes" do
       expect(@user1).to be_valid
     end
+
     it "is not valid without a name" do
       user = build(:user, name: nil)
       expect(user).to_not be_valid
     end
+
     it "is not valid without an email" do
       user = build(:user, email: nil)
       expect(user).to_not be_valid
     end
     
+    it "is not valid without a password" do
+      user = build(:user, password: nil)
+      expect(user).to_not be_valid
+    end
   end
 end
