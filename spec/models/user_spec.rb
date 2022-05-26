43ca2b26a0ad0cb85349cd6e5cc4214506eb48d3
Samuel Mwape <43408618+Mwapsam@users.noreply.github.com>
2022-05-26 17:44:33 +0200

    Validates user attributes

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,4 +4,11 @@ RSpec.describe User, :type => :model do
   before(:all) do
     @user1 = create(:user)
   end
+
+  describe "validations" do
+    it "is valid with valid attributes" do
+      expect(@user1).to be_valid
+    end
+    
+  end
 end
