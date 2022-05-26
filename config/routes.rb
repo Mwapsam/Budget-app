1f76b23491c694d7c9d5c8fc51528ea864b38296
Samuel Mwape <43408618+Mwapsam@users.noreply.github.com>
2022-05-26 10:10:49 +0200

    Configure routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,22 @@
 Rails.application.routes.draw do
   devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  root "catagories#index"
+  devise_scope :user do
+    authenticated :user do
+      root 'categories#index', as: :authenticated_root
+    end
+
+    unauthenticated do
+      root 'user#index', as: :unauthenticated_root
+    end
+  end
+
+  get 'categories/most_recent_list'
+  get 'categories/most_ancient_list'
+
+  resources :categories, only: %i[index new create show] do
+    resources :payments, only: %i[index new create]
+  end
+
+  resources :users, only: %i[index]
 end
