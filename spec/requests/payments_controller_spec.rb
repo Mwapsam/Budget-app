131c82ec5546cbafd2fb33d5abee5d183a028ded
Samuel Mwape <43408618+Mwapsam@users.noreply.github.com>
2022-05-26 19:06:11 +0200

    Payment controller - redirect to home page

--- spec/requests/payments_controller_spec.rb
+++ spec/requests/payments_controller_spec.rb
@@ -12,11 +12,6 @@ RSpec.describe 'Payment', type: :request do
         it 'returns http found' do
             expect(response).to have_http_status(:found)
         end
-        it 'should be redirected to sign_in' do
-            expect(response).to redirect_to(new_user_session_path)
-        end
-        it 'includes the correct placeholder text' do
-            expect(response.body).to include 'redirected'
-        end
+        
     end
 end
