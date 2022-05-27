7e816d6155ac8ae7483338618eebca98e03f95d2
Samuel Mwape <43408618+Mwapsam@users.noreply.github.com>
2022-05-27 12:05:01 +0200

    TDD - Category show is styled

--- spec/features/category_show_spec.rb
+++ spec/features/category_show_spec.rb
@@ -30,6 +30,13 @@ RSpec.describe 'Category show page', type: :feature do
             expect(page).to have_current_path(category_payments_path(@category))
         end
         
+        scenario 'confirm that all category names are properly displayed' do
+            @category = Category.all
+            @category.each do |category|
+                expect(page).to have_content(category.name)
+            end
+        end
+
         
     end
 end
