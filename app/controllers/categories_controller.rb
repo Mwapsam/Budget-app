ef3fbfe6e80b4d582fb1f46bbdb8870d2511d590
Samuel Mwape <43408618+Mwapsam@users.noreply.github.com>
2022-05-28 10:17:25 +0200

    Added delete on category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -20,12 +20,34 @@ class CategoriesController < ApplicationController
     @catagory = Category.new
   end
 
+  def edit; end
+
   def create
     add_category = current_user.categories.new(category_params)
     if add_category.save
       redirect_to categories_path, notice: 'Category was successfully added'
     else
       render :new, alert: 'Failed to add category'
+    end
+  end
+
+  def destroy
+    @user = current_user
+    @category = Category.find(params[:id])
+
+    if @category.destroy
+      redirect_to categories_path, notice: 'Category was successfully deleted'
+    else
+      render :index, alert: 'Failed to delete category'
+    end
+  end
+
+  def update
+    @category = Category.find(params[:id])
+    if @category.update(category_params)
+      redirect_to categories_path, notice: 'Category was successfully updated'
+    else
+      render :edit, alert: 'Failed to update category'
     end
   end
 
